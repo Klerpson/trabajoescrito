c7cf6cbaad81cd7bf08fd2813946a712868595c6
Julian DW <klerpson@gmail.com>
2023-05-23 17:55:29 -0500

    layout header

---
+++ _site/js/slider.js
@@ -0,0 +1,39 @@
+const carousel = document.querySelector(".slide-container");
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".gallery-slider span");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 24;
+
+arrowIcons.forEach(icon =>{
+  icon.addEventListener("click", () => {
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+  })
+})
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
